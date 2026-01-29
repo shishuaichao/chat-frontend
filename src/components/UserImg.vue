755b84759da29914a1a0de70e0b9057870db7069
xiaoxia <shishuaichao578@163.com>
2026-01-29 13:02:05 +0800

    userimg

---
+++ src/components/UserImg.vue
@@ -0,0 +1,90 @@
+<template>
+  <div 
+    class="user_img_box"
+    :class="{'mini': size == 'mini', 'normal': size == 'normal', 'large': size == 'large'}"
+    >
+    <div 
+      class="img_box"
+      v-if="avatarList.length == 1 || avatar">
+      <img :src="avatar" alt="" class="avatar avatar_1" />
+    </div>
+    <div 
+      class="img_box img_box_mul" 
+      v-if="avatarList.length > 1"
+      >
+      <img 
+        alt="avatar" 
+        v-for="(v, index) in avatarList" 
+        :key="index" 
+        :src="v" 
+        class="avatar"
+        :class="{
+          'avatar_1': avatarList.length == 1,
+          'avatar_234': avatarList.length > 1 && avatarList.length <= 4,
+          'avatar_5-9': avatarList.length >= 5 && avatarList.length <= 9,
+        }"
+        />
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  avatarList: {
+    type: Array,
+    default: () => [],
+  },
+  avatar: {
+    type: String,
+    default: '',
+  },
+  size: {
+    type: String,
+    default: 'normal',
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.user_img_box {
+  display: inline-block;
+  &.mini {
+    width: 40px;
+    height: 40px;
+  }
+  &.normal {
+    width: 48px;
+    height: 48px;
+  }
+  &.large {
+    width: 64px;
+    height: 64px;
+  }
+}
+.img_box {
+  box-sizing: border-box;
+  background-color: #f0f0f0;
+  border-radius: 4px;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  .avatar_1 {
+    width: 100%;
+    height: 100%;
+  }
+  .avatar_234 {
+    width: 42%;
+    height: 42%;
+  }
+  .avatar_5-9 {
+    width: 30%;
+    height: 30%;
+  }
+}
+.img_box_mul {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+  gap: 2px;
+}
+</style>
--- src/views/tabBars/TabSearch.vue
+++ src/views/tabBars/TabSearch.vue
@@ -1,12 +1,42 @@
 
 <template>
-  <div>
-    搜索
+  <div class="search_box">
+    <UserImg :avatar="avatar" size="mini" />
+    <UserImg :avatar="avatar" size="normal" />
+    <UserImg :avatar="avatar" size="large" />
+    <UserImg :avatarList="avatarList.slice(0,2)" size="mini" />
+    <UserImg :avatarList="avatarList.slice(0,3)" :size="size" />
+    <UserImg :avatarList="avatarList.slice(0,4)" :size="size" />
+    <UserImg :avatarList="avatarList.slice(0,5)" size="large" />
+    <UserImg :avatarList="avatarList.slice(0,6)" :size="size" />
+    <UserImg :avatarList="avatarList.slice(0,7)" :size="size" />
+    <UserImg :avatarList="avatarList.slice(0,8)" :size="size" />
+    <UserImg :avatarList="avatarList.slice(0,9)" :size="size" />
   </div>
 </template>
 <script setup>
+import UserImg from '@/components/UserImg.vue'
+import { IMG_REAL_URL } from '@/utils/constant'
+import { ref } from 'vue'
 
+
+const size = ref('normal')
+const avatar = ref(`${IMG_REAL_URL}23`)
+const avatarList = ref([
+  `${IMG_REAL_URL}23`, 
+  `${IMG_REAL_URL}24`, 
+  `${IMG_REAL_URL}25`, 
+  `${IMG_REAL_URL}26`,
+  `${IMG_REAL_URL}27`,
+  `${IMG_REAL_URL}28`,
+  `${IMG_REAL_URL}29`,
+  `${IMG_REAL_URL}30`,
+  `${IMG_REAL_URL}31`,
+])
+// avatarList.value.slice(3)
 </script>
 <style scoped lang="scss">
-
+.search_box {
+  padding: 20px;
+}
 </style>
